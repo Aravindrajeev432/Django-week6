bfc0323b200dadb9db7ecd6f46c4fc4ad0e27811
Aravind R <ndarvi48332@gmail.com>
2022-07-13 11:08:43 +0530

    user registerd and login fixing ongoing on views

--- login_system/models.py
+++ login_system/models.py
@@ -8,7 +8,7 @@ class Users(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     email = models.EmailField(max_length = 254)
-    phone = models.IntegerField()
+    phone = models.BigIntegerField()
     passw =models.CharField(max_length=200)
     address =models.CharField(max_length=300)
     city = models.CharField(max_length=200)
--- login_system/views.py
+++ login_system/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.views.decorators.cache import cache_control
 from .models import Users
+import sys
 # Create your views here.
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def index(request):
@@ -12,11 +13,24 @@ def index(request):
         password = request.POST.get('pass')
         print(username)
         print(password)
+        # Raw Query
+        user_log=Users.objects.raw('SELECT login_system_users.uid,login_system_users.first_name FROM login_system_users')
+        print("size of raw query variable is", end='')
+        print(sys.getsizeof(user_log))
+        for i in user_log:
+            if username==i.first_name and password==i.passw:
+                print(i.first_name)
+                print("successs")
+                break
+            else :
+                print("failed")
+        # print(user_log)
     print("index")
     return render(request,'index.html')
+
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def reg(request):
-    print("home")
+    print("reg")
     if request.method == 'POST':
         
         fname = request.POST.get('fname')
@@ -38,11 +52,11 @@ def reg(request):
         print("city-"+city)
         print("state-"+str(staten))
         print("zip-"+str(zip))
+        user_tabel=Users(first_name=fname,last_name=lname,email=email,phone=phone,passw=passw,address=address,city=city,state=staten,zip=zip)
+        user_tabel.save()
+        return redirect(index)
 
-
-        # return render(request,'home.html')
-
-    return render(request,'home.html')
+    return render(request,'reg.html')
 def home(request):
     user_details = Users.objects.all()
     return render(request,'home.html',{'u_details':user_details})
